42bc48dac3afd8fa41b5b1f31b2ccf3c101b1b31
garasubo <garasubo@gmail.com>
2024-01-28 15:46:55 +0900

    initial commit

---
+++ rt/src/lib.rs
@@ -0,0 +1,104 @@
+#![cfg_attr(not(test), no_std)]
+
+use core::panic::PanicInfo;
+use core::ptr;
+use core::ptr::addr_of_mut;
+
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+    extern "C" {
+        static mut _sbss: u8;
+        static mut _ebss: u8;
+        static mut _sdata: u8;
+        static mut _edata: u8;
+        static _sidata: u8;
+    }
+
+    let sbss = addr_of_mut!(_sbss);
+    let ebss = addr_of_mut!(_ebss);
+    let sdata = addr_of_mut!(_sdata);
+    let edata = addr_of_mut!(_edata);
+    let sidata = &_sidata as *const u8;
+
+    let count = ebss as usize - sbss as usize;
+    ptr::write_bytes(sbss, 0, count);
+
+    let count = edata as usize - sdata as usize;
+    ptr::copy_nonoverlapping(sidata, sdata, count);
+
+    extern "Rust" {
+        fn main() -> !;
+    }
+
+    main()
+}
+
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[macro_export]
+#[cfg(not(test))]
+macro_rules! entry {
+    ($path:path) => {
+        #[export_name = "main"]
+        pub unsafe fn __main() -> ! {
+            let f: fn() -> ! = $path;
+
+            f()
+        }
+    };
+}
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub union Vector {
+    pub reserved: u32,
+    pub handler: unsafe extern "C" fn(),
+}
+
+extern "C" {
+    fn NMI();
+    fn HardFault();
+    fn MemManage();
+    fn BusFault();
+    fn UsageFault();
+    fn PendSV();
+    fn SysTick();
+}
+
+#[link_section = ".vector_table.exceptions"]
+#[no_mangle]
+pub static EXCEPTIONS: [Vector; 14] = [
+    Vector { handler: NMI },
+    Vector { handler: HardFault },
+    Vector { handler: MemManage },
+    Vector { handler: BusFault },
+    Vector {
+        handler: UsageFault,
+    },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { handler: SVCall },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { handler: PendSV },
+    Vector { handler: SysTick },
+];
+
+#[no_mangle]
+pub extern "C" fn DefaultExceptionHandler() {
+    loop {}
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn SVCall() {
+    todo!()
+}
+
