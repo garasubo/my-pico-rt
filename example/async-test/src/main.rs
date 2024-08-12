062b3e63e87cf32bb870b95463d5c296c9f06a3d
garasubo <garasubo@gmail.com>
2024-08-12 17:39:48 +0900

    add async example

---
+++ example/async-test/src/main.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use pico_hal::gpio::Gpio;
+use pico_hal::resets::Resets;
+use pico_rt::entry;
+use pico_rt::task::{SimpleExecutor, Task};
+use util::linked_list::ListItem;
+
+entry!(main);
+
+async fn async_test() -> bool {
+    true
+}
+
+async fn example_task(gpio: Gpio) {
+    let result = async_test().await;
+    if result {
+        gpio.set_high(6);
+    }
+}
+
+pub fn main() -> ! {
+    let resets = Resets::new();
+    let gpio = Gpio::new();
+    gpio.wait_gpio_reset_done(&resets);
+    gpio.set_output_enable(6);
+
+    let mut executor = SimpleExecutor::new();
+    let task = Task::new(example_task(gpio));
+    let mut item = ListItem::create(task);
+    executor.spawn(&mut item);
+    executor.run();
+
+    loop {
+    }
+}
