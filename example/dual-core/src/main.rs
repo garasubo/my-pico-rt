e7e9a2959370d8a5b9d08a8c31ad991aae0dd249
garasubo <garasubo@gmail.com>
2024-08-25 15:51:01 +0900

    implement blink led

--- example/dual-core/src/main.rs
+++ example/dual-core/src/main.rs
@@ -5,23 +5,10 @@ extern crate alloc;
 
 use pico_hal::gpio::Gpio;
 use pico_hal::resets::Resets;
-use pico_hal::{clock, uart};
+use pico_hal::{clock, systick, uart};
 use pico_rt::{boot_core1, entry};
-use pico_rt::task::{SimpleExecutor, Task};
-use util::linked_list::ListItem;
 
 entry!(main);
-
-async fn async_test() -> bool {
-    true
-}
-
-async fn example_task(gpio: Gpio) {
-    let result = async_test().await;
-    if result {
-        gpio.set_high(6);
-    }
-}
 
 pub extern "C" fn MainCore1Func() {
     let resets = Resets::new();
@@ -45,6 +32,16 @@ pub fn main() -> ! {
     gpio.set_output_enable(6);
     boot_core1();
     gpio.set_high(6);
+    let mut flag = true;
+    systick::init(1000 * 1000);
     loop {
+        while !systick::check_counted() {}
+        if flag {
+            gpio.set_low(6);
+            flag = false;
+        } else {
+            gpio.set_high(6);
+            flag = true;
+        }
     }
 }
